79ba7b71ba20bcec1ee4c4a071c1160efcc313a8
jairo neder monassa <84083197+desenvolvedorjairomonassa@users.noreply.github.com>
2024-06-18 16:52:45 -0300

    Create leapyearv2.py

---
+++ leapyearv2.py
@@ -0,0 +1,12 @@
+# calcula o ano bissexto (leap year)
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+      return True
+  else:
+    return False
