0811a761c0b925b9aba924e5d2b23deb9c67b5a1
jairo neder monassa <84083197+desenvolvedorjairomonassa@users.noreply.github.com>
2024-06-17 22:26:50 -0300

    Update leapYear.py

--- leapYear.py
+++ leapYear.py
@@ -1,8 +1,10 @@
-### check if year is leap year
+### check if year is leap year in the gregorian calendar
 
 def is_leap(year):
+    year = int(year)
     leap = False
-    
-    leap = (year//100 == 0) and (year//4 == 0)
-    
+    century = (year % 100 == 0)
+    leap = (year>=1900) and (year<= 100000)
+
+    leap = leap and ((century and year % 400 == 0) or (not century and year % 4 == 0))
     return leap
